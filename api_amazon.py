aeb7b8647c7a822c21e0e3557b3285c0695a37b2
alexisgonzales2728-byte <alexisgonzales2728@gmail.com>
2025-11-27 01:36:02 -0600

    creo ya quedo

--- api_amazon.py
+++ api_amazon.py
@@ -153,4 +153,6 @@ def check_multiple_cards():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    # Para producción en Northflank:
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=port)
